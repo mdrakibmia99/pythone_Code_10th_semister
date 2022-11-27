7269eaf9b0fb671a08766dde71cb9ed1da604478
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-11-27 23:36:31 +0600

    6_Create an array of 8 items array to another variable then copy this

---
+++ Assignment/6_array_to_copy_value.py
@@ -0,0 +1,7 @@
+
+#6_Create an array of 8 items array to another variable then copy this 
+import numpy as np
+myArray=np.arange(10,18)
+copyArray=myArray.copy()
+print("\nOriginal Array=",myArray)
+print("\nCopy Array=",copyArray)
